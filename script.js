29e3b48d7d6784a8b7080be5ed22af3fe95c329b
Jeetesh  Kumar <98549835+Kumarj67@users.noreply.github.com>
2022-12-27 06:27:12 +0000

    Adding Jquery logic

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// function fetchRandomDogImage(){
+//     var xhrRequest=new XMLHttpRequest();
+//     xhrRequest.onload=function(){
+//     console.log(xhrRequest.response);
+//      var responseJSON= JSON.parse(xhrRequest.response);
+//       // JSON.stringify to convert json to string
+//       var imageUrl=responseJSON.message;
+//     $('#dog-image').attr('src',imageUrl)
+//     };
+//     xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
+//     xhrRequest.send();
+// }
+
+
+// $('#fetch-dog-image-button').click(fetchRandomDogImage);
 function fetchRandomDogImage(){
-    var xhrRequest=new XMLHttpRequest();
-    xhrRequest.onload=function(){
-    console.log(xhrRequest.response);
-     var responseJSON= JSON.parse(xhrRequest.response);
-      // JSON.stringify to convert json to string
-      var imageUrl=responseJSON.message;
-    $('#dog-image').attr('src',imageUrl)
-    };
-    xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
-    xhrRequest.send();
+
+$.ajax({
+    url:'https://dog.ceo/api/breeds/image/random',
+    method:'GET',
+    success: function(data){
+     var imageUrl=data.message;
+     $('#dog-image').attr('src',imageUrl)
+    }
+})
 }
-
-
 $('#fetch-dog-image-button').click(fetchRandomDogImage);
